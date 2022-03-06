2b14015983dedb59fea7928d239c98065644fa00
danieluhm2004 <iam@dan.al>
2022-03-06 17:28:11 +0000

    add: test code

---
+++ src/index.test.js
@@ -0,0 +1,132 @@
+'use strict';
+
+const childProcess = require('child_process');
+const ServerlessDoppler = require('.');
+const { randomBytes } = require('crypto');
+
+jest.mock('child_process');
+describe('Check serverless-doppler plugin', () => {
+  let plugin;
+  beforeEach(() => {
+    plugin = new ServerlessDoppler({
+      cli: { log: console.log },
+      service: { custom: { doppler: {} } },
+    });
+  });
+
+  test('Set environments', () => {
+    const environments = {};
+    for (let i = 0; i < 10; i++) {
+      const key = `${randomBytes(4).toString('hex')}`;
+      environments[key] = key;
+    }
+
+    plugin.setEnvironments(environments);
+    expect(plugin.serverless.service.provider.environment).toEqual(
+      environments
+    );
+  });
+
+  test('Move enclave to parent', () => {
+    const project = `${randomBytes(4).toString('hex')}`;
+    const config = `${randomBytes(4).toString('hex')}`;
+    const customProperty = {
+      'enclave.project': project,
+      'enclave.config': config,
+    };
+
+    expect(plugin.moveEnclaveToParent(customProperty)).toEqual({
+      ...customProperty,
+      project,
+      config,
+    });
+  });
+
+  test('Get headers from property', () => {
+    expect(plugin.getHeadersFromProperty()).toEqual({
+      'User-Agent': 'serverless-doppler',
+    });
+  });
+
+  test('Get info from property', () => {
+    const property = { apiHost: `${randomBytes(4).toString('hex')}` };
+    expect(plugin.getInfoFromProperty(property)).toEqual({
+      method: 'GET',
+      baseURL: property.apiHost,
+      url: '/v3/configs/config/secrets/download',
+    });
+  });
+
+  test('Get doppler property by Cli (Empty)', () => {
+    childProcess.execSync.mockImplementation((command) => {
+      expect(command).toEqual(`doppler configure --json`);
+      return JSON.stringify({});
+    });
+
+    expect(plugin.getDopplerPropertyByCli()).toEqual({
+      apiHost: undefined,
+      config: undefined,
+      token: undefined,
+      project: undefined,
+      includeDynamicSecrets: undefined,
+    });
+  });
+
+  test('Get doppler property by Cli (With value)', () => {
+    const customConfig = {
+      apiHost: `${randomBytes(4).toString('hex')}`,
+      config: `${randomBytes(4).toString('hex')}`,
+      token: `${randomBytes(4).toString('hex')}`,
+      project: `${randomBytes(4).toString('hex')}`,
+    };
+
+    childProcess.execSync.mockImplementation((command) => {
+      expect(command).toEqual(`doppler configure --json`);
+      return JSON.stringify({ '/': customConfig });
+    });
+
+    expect(plugin.getDopplerPropertyByCli()).toEqual(customConfig);
+  });
+
+  test('Get auth from property', () => {
+    const token = `${randomBytes(4).toString('hex')}`;
+    expect(plugin.getAuthFromProperty({ token })).toEqual({ username: token });
+  });
+
+  test('Get params from property', () => {
+    const format = 'json';
+    const project = `${randomBytes(4).toString('hex')}`;
+    const config = `${randomBytes(4).toString('hex')}`;
+    const includeDynamicSecrets = true;
+    const customProperty = { project, config, includeDynamicSecrets };
+    expect(plugin.getParamsFromProperty(customProperty)).toEqual({
+      include_dynamic_secrets: includeDynamicSecrets,
+      format,
+      project,
+      config,
+    });
+  });
+
+  test('Get doppler property by Serverless (Empty)', () => {
+    expect(plugin.getDopplerPropertyByServerless()).toEqual({
+      apiHost: undefined,
+      config: undefined,
+      token: undefined,
+      project: undefined,
+      includeDynamicSecrets: undefined,
+    });
+  });
+
+  test('Get doppler property by Serverless (With value)', () => {
+    const customConfig = {
+      apiHost: `${randomBytes(4).toString('hex')}`,
+      config: `${randomBytes(4).toString('hex')}`,
+      token: `${randomBytes(4).toString('hex')}`,
+      project: `${randomBytes(4).toString('hex')}`,
+      includeDynamicSecrets: true,
+    };
+
+    plugin.serverless.service.custom.doppler = customConfig;
+    expect(plugin.getDopplerPropertyByServerless()).toEqual(customConfig);
+  });
+});
